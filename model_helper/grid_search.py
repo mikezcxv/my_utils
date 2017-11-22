e182ac5e1d52e9d811bf1c65d702123753dc1f15
git <MyGit>
2017-11-22 18:43:45 +0200

    Add Bayesian Draft optimizer

--- model_helper/grid_search.py
+++ model_helper/grid_search.py
@@ -59,3 +59,42 @@ def get_optimal_params(df, target, oos=.8):
         print("%s: %r" % (param_name, best_parameters[param_name]))
 
     return clf
+
+
+# def draft_bayes_opt():
+#     from bayes_opt import BayesianOptimization
+#
+#     def xgb_evaluate(min_child_weight, colsample_bytree, max_depth, subsample, gamma, alpha):
+#         params['min_child_weight'] = int(min_child_weight)
+#         params['cosample_bytree'] = max(min(colsample_bytree, 1), 0)
+#         params['max_depth'] = int(max_depth)
+#         params['subsample'] = max(min(subsample, 1), 0)
+#         params['gamma'] = max(gamma, 0)
+#         params['alpha'] = max(alpha, 0)
+#
+#         #     , target_train = tr.ix[:, tr.columns != target], tr.ix[:, target]
+#
+#         dtrain = xgb.DMatrix(df_train, target_train)
+#         cv_result = xgb.cv(params, dtrain, num_boost_round=num_rounds, nfold=5,
+#                            seed=random_state, callbacks=[xgb.callback.early_stop(30)])
+#
+#         # TODO Redefine with own cv avg
+#
+#         return -cv_result['test-auc-mean'].values[-1]
+#
+#     num_rounds = 300
+#     random_state = 42
+#     num_iter = 10
+#     init_points = 5
+#     params = {'eta': 0.1, 'silent': 1, 'eval_metric': 'auc', 'verbose_eval': True, 'seed': random_state}
+#
+#     xgbBO = BayesianOptimization(xgb_evaluate, {'min_child_weight': (1, 20),
+#                                                 'colsample_bytree': (0.1, 1),
+#                                                 'max_depth': (3, 4, 5),
+#                                                 'subsample': (0.5, 1),
+#                                                 'gamma': (0, 10),
+#                                                 'alpha': (0, 10),
+#                                                 })
+#
+#     xgbBO.maximize(init_points=init_points, n_iter=num_iter)
+
