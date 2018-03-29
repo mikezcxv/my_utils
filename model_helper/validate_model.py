0b7a358444d7af724ad8569e930cb207898994f0
Mykola Mykytenko <MyGit>
2018-03-29 14:41:42 +0300

    Add confidence interval checking

--- model_helper/validate_model.py
+++ model_helper/validate_model.py
@@ -1,9 +1,32 @@
+import pandas as pd
 import scipy as sp
 import numpy as np
 import seaborn as sns
+import statsmodels
 
 
-def mean_confidence_interval(data, confidence=0.99):
+def get_binomial_ci(cnt_positive, cnt_total, ci=0.95, pr=3, simple=True, min_total=30, show_plt=False):
+    a = statsmodels.stats.proportion.proportion_confint(cnt_positive, cnt_total, alpha=1 - ci, method='wilson')
+    ratio = cnt_positive / cnt_total
+
+    if simple:
+        b = round(ratio, pr)
+    else:
+        b = str(round(ratio, pr)) + ' : %d from %d' % (cnt_positive, cnt_total)
+
+    if cnt_total < min_total:
+        return np.NaN, np.NaN
+    else:
+        #         if show_plt:
+        #             er = [[ratio - float(a[0])], [float(a[1]) - ratio]]
+        #             plt.errorbar(x=np.arange([1]), y=ratio, yerr=er, fmt='ro--',
+        #                          linewidth=1, ecolor=colors[i], capsize=3, capthick=1)
+        #             plt.show()
+
+        return str(round(a[0], pr)) + ' .. ' + str(round(a[1], pr)), b
+
+
+def get_normal_ci(data, confidence=0.99):
     a = 1.0 * np.array(data)
     n = len(a)
     m, se = np.mean(a), sp.stats.sem(a)
@@ -12,7 +35,7 @@ def mean_confidence_interval(data, confidence=0.99):
 
 
 def analyze_confidence(data: list):
-    l = []
+    l = data
     # for k in list_predictions.keys():
     #     a = roc_auc_score(rx.target_test, list_predictions[k])
     #     l.append(a)
@@ -22,7 +45,7 @@ def analyze_confidence(data: list):
     # 76.06 +- 0.65; [74.84 .. 77.17]; range: 2.33; median: 76.00
     # 76.06 +- 0.63; [74.79 .. 77.17]; range: 2.38; median: 76.00
 
-    n = len(data)
+    n = len(l)
 
     s, m = np.std(l), np.mean(l)
     print('Mean: %.3f; Std: %.5f; Range: %.5f' % (m * 100, s * 100, round(max(l) - min(l), 7)))
@@ -37,14 +60,87 @@ def analyze_confidence(data: list):
 
     print('[%.2f .. %.2f] - 95%% mean range' % (m - 2.56 * s / math.sqrt(n), m + 2.56 * s / math.sqrt(n)))
     print('[%.2f .. %.2f] - 99%% mean range' % (m - 3 * s / math.sqrt(n), m + 3 * s / math.sqrt(n)))
-    ci_95 = mean_confidence_interval(l, confidence=0.95)
-    ci_99 = mean_confidence_interval(l, confidence=0.99)
+    ci_95 = get_normal_ci(l, confidence=0.95)
+    ci_99 = get_normal_ci(l, confidence=0.99)
 
     print('***')
     print('[%.2f .. %.2f] - 95%% mean range\'' % (ci_95[1] * 100, ci_95[2] * 100))
     print('[%.2f .. %.2f] - 99%% mean range\'' % (ci_99[1] * 100, ci_99[2] * 100))
 
     sns.boxplot(l)
+
+
+pd.set_option('display.max_rows', 500)
+# pd.set_option('display.max_columns', 500)
+# pd.set_option('display.width', 1000)
+
+
+def get_ci(row, ci=0.95, pr=3, simple=True, tr=30):
+    a = statsmodels.stats.proportion.proportion_confint(row['clc_id'], row['imp_id'], alpha=1 - ci, method='wilson')
+    b = round(row['clc_id'] / row['imp_id'], pr) if simple else str(
+        round(row['clc_id'] / row['imp_id'], pr)) + ' for %d impressions' % row['imp_id']
+    if row['imp_id'] < tr:
+        return (np.NaN, np.NaN)
+    else:
+        return (str(round(a[0], pr)) + ' .. ' + str(round(a[1], pr)), b)
+
+
+# # df.loc[df.brand == 'RACQ']['ranking'].value_counts()
+# info = df[['brand', 'ranking', 'imp_id', 'clc_id']]\
+#     .loc[df.brand.isin(comparison_brands)]\
+#     .groupby(('ranking', 'brand')).agg('count')\
+#     .apply(lambda row: get_binomial_ci(row['clc_id'], row['imp_id'], ci=0.95, simple=True), axis=1)
+
+
+def plot_error_bar(info, n_brands=4, n_ranks=5):
+    # plt.plot(info['clc_id'][:n_brands * n_ranks].reshape((-1, n_brands)))
+
+    # er = [[0.002, 0.009], [0.039, 0.047]]
+
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 8))
+    # ax.set_facecolor((1.0, 1.0, 1.0))
+    fig.set_facecolor((1.0, 1.0, 1.0))
+
+    colors = ['k', 'r', 'g', 'b', 'y', 'b', 'c', 'm', 'k']
+
+    labels = []
+    for i in range(n_brands):
+        l = info['imp_id'].values[:n_brands * n_ranks].reshape((-1, n_brands))[:, i]
+        labels.append(info.index.get_level_values(1)[i])
+        bottom, upper = [], []
+        for q in l:
+            it = q.split(' .. ')
+            bottom.append(float(it[0]))
+            upper.append(float(it[1]))
+
+        y = info['clc_id'][:n_brands * n_ranks].reshape((-1, n_brands))[:, i]
+        er = [y - bottom, upper - y]
+
+        ax.errorbar(x=np.linspace(1, n_ranks, n_ranks) + i / 20, y=y,
+                    yerr=er, fmt=colors[i] + 'o--', linewidth=1, ecolor=colors[i], capsize=3, capthick=1)
+
+    plt.title(r'$\bf%s$ CTR comparison' % vertical.replace(' ', '\ '))
+    plt.legend(labels)
+    plt.xlabel('Item ranking')
+
+
+# from math import sqrt
+#
+# def confidence(clicks, impressions):
+#     n = impressions
+#     if n == 0: return 0
+#     z = 1.96 #1.96 -> 95% confidence
+#     phat = float(clicks) / n
+#     denorm = 1. + (z*z/n)
+#     enum1 = phat + z*z/(2*n)
+#     enum2 = z * sqrt(phat*(1-phat)/n + z*z/(4*n*n))
+#     return (enum1-enum2)/denorm, (enum1+enum2)/denorm
+#
+# def wilson(clicks, impressions):
+#     if impressions == 0:
+#         return 0
+#     else:
+#         return confidence(clicks, impressions)
 
 
 # Draft for unbalanced
